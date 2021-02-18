9ab26abeb6a67109c80a6aa913f830947612fa01
tiger502 <tiger502@vt.edu>
2021-02-18 17:00:52 -0500

    Added doc test

--- tests/test.py
+++
@@ -1,7 +0,0 @@
-from docopt import docopt
-import test_runner
-
-if __name__ == '__main__':
-    testing = docopt(test_runner.doc, version="test 2.0", help_message=False,
-                     argv=test_runner.argv)
-    print(testing)
