f157e39bd2372d439e0bdf0533fcc028dc8892c2
Kevin Williams <williamskt98@gmail.com>
2021-03-19 17:08:40 -0400

    Finish match functions and tree structure

---
+++ examples/docopt_util.py
@@ -0,0 +1,313 @@
+"""
+  This module is for the helper functions of docopt.py.
+  Contains the function for processing docstrings and
+  formatting the print strings to user
+"""
+import warnings
+import math
+
+
+# Main Controller for processing the docstring.
+def processing_string(doc, help_message, version):
+    """
+     Args:
+        doc: docstring pass from the main function.
+        help_message: to tell docopt whether user want to display help message when
+                      the program executes.
+        version: the version string pass from main function.
+        version: programmers can specify the version of the project and display to user.
+    Returns:
+        usage.split("\n"): returns the array of usage patterns.
+        options.split("\n"): returns the array of options from docstring.
+
+    >>> doc1 = 'Perfect' \
+    >>>
+    >>>        'Usage:' \
+    >>>          'naval_fate.py ship new <name>...' \
+    >>>
+    >>>        'Options:' \
+    >>>           '-h --help --helping Show this screen.' \
+    >>>           '--sorted  Show sorted.'
+    >>>
+    >>> processing_string(doc=doc1, help_message=False, version="test 2.0")
+    ['Usage:', '  naval_fate.py ship new <name>...'], \
+    ['Options:', '  -h --help --helping Show this screen.', '  --sorted  Show sorted.']
+    """
+
+    if doc is None:
+        warnings.warn('No docstring found')
+        return None
+    name, usage, options, display_help = get_usage_and_options(doc, version)
+    check_warnings(usage, options)
+    if help_message:
+        print(display_help)
+    return usage.split("\n"), options.split("\n")
+
+
+# Helper function for getting usage, options and name strings from doc
+def get_usage_and_options(doc, version):
+    """
+     Args:
+        doc: docstring that passed from main function.
+        version: the version string pass from main function.
+    Returns:
+        name: returns the strings of name of program.
+        usage: returns the strings of usage patterns.
+        options: returns the strings of options that received from docstring
+
+    >>> doc1 = 'Perfect' \
+    >>>
+    >>>        'Usage:' \
+    >>>          'naval_fate.py ship new <name>...' \
+    >>>
+    >>>        'Options:' \
+    >>>           '-h --help --helping Show this screen.' \
+    >>>           '--sorted  Show sorted.'
+    >>>
+    >>> get_usage_and_options(doc1)
+    "Perfect",
+    "Usage: \
+    naval_fate.py ship new <name>...", \
+    "Options: \
+    -h --help --helping Show this screen. \
+    --sorted  Show sorted."
+    """
+
+    usage = ""
+    options = ""
+    partition_string = doc.strip().split('\n\n')
+
+    name = partition_string[0].strip()
+    partition_string.pop(0)
+
+    if "Usage:" in name:
+        usage = name
+        name = ""
+    else:
+        for element in partition_string:
+            if 'Usage:' in element.strip():
+                usage = element
+                partition_string.remove(element)
+
+    if version is not None and len(name) > 0:
+        display_help = name + "\n\n" + "Version:\n" + version + "\n\n" + usage + "\n\n" + \
+                       "\n\n".join(partition_string) + "\n\n"
+    elif version is not None:
+        display_help = "Version:\n  " + version + "\n\n" + usage + "\n\n" + "\n\n".join(
+            partition_string) + "\n\n"
+    elif len(name) > 0:
+        display_help = name + "\n\n" + usage + "\n\n" + "\n\n".join(
+            partition_string) + "\n\n"
+    else:
+        display_help = usage + "\n\n" + "\n\n".join(partition_string) + '\n\n'
+
+    for element in partition_string:
+        if element.strip()[:1] == '-' or 'Options:' in element.strip():
+            options = element
+            partition_string.remove(element)
+    print(name)
+    print(usage)
+    print(options)
+    return name, usage, options, display_help
+
+
+# Will display warning to the user program when missing parts
+def check_warnings(usage, options):
+    """ Function for testing whether the docstring contains a usage part and a options part.
+     Args:
+        usage: a string the retrieve from the docstring.
+        options: a string that retrieve from the docstring.
+    Returns:
+        returns 1 if no usage pattern found, returns 2 if no options found,
+        and returns 0 if everything is ok in docstring
+    Raises:
+        Warnings: If no usages or options contained in the docstring.
+
+    >>> check_warnings(usage="Usages: ...", options="")
+    0
+    >>> check_warnings(usage="", options="Options: ...")
+    1
+    >>> check_warnings(usage="Usages: ...", options="")
+    2
+    """
+
+    if len(usage) == 0:
+        warnings.warn('No usage indicated from docstring')
+        return 1
+    if len(options) == 0:
+        warnings.warn('No options indicated from docstring')
+        return 2
+    return 0
+
+
+# Main function for building output strings to user
+def print_output_dictionary(usage_dic, options_dic):
+    """
+    Args:
+        usage_dic: the original usage dictionary from main function.
+        options_dic: the original options dictionary from main function.
+    Returns:
+        dictionary_total: the final dictionary object that built from usage pattern and options.
+        return the formatted json like dictionary string to user.
+
+    >>> input1 = {'1': True, '2': 'haha', '3': False, '4': True, '5': 'haha'}
+    >>> u_dic = {'usage1': 'x', 'usage2': 'y'}
+    >>> dic_total, res = print_output_dictionary(usage_dic=u_dic, options_dic=input1)
+    >>> assert dic_total == {**usage_dic, **dic_total}
+    {'usage1': 'x'
+     'usage2': 'y'
+     '1': True
+     '2': 'haha'
+     '3': False
+     '4': True
+     '5': 'haha'
+    }
+    """
+
+    dictionary_total = {}
+    dictionary_total.update(usage_dic)
+    dictionary_total.update(options_dic)
+    dic_list = list(dictionary_total)
+    length = len(dictionary_total)
+    if length > 24:
+        rows = math.ceil(length / 3)
+    else:
+        rows = 8
+    return dictionary_total, output_formatter(rows, length, dic_list, dictionary_total)
+
+
+# A helper function for display a nice looking dictionary to the user
+def output_formatter(rows, length, dic_list, dictionary_total):
+    """
+    Args:
+        rows: count for how many rows needed for output dictionary.
+        length: the total length for the output usage and options dictionary.
+        dic_list: reformat the dictionary into a array.
+        dictionary_total: combined dictionary (usage dic + options dic).
+    Returns:
+        returns the string from display or an array for testing.
+
+    >>> dic = {'--helping': True, '--sorted': True, '--output': 'ttt.pdf', '--version': False,
+    >>>        '--speed': 10, '--moored': True, '--drifting': None, '--rr': False, '--aaa': 20.9,
+    >>>        '--yyy': False}
+    >>> d_list = list(dic)
+    >>> output_formatter(rows=4, length=len(dic_list), dic_list=dic_list, dictionary_total=dic)
+    "{'--helping': True         '--speed': 10          '--aaa': 20.9\n" + \
+    " '--sorted': True          '--moored': True       '--yyy': False\n" + \
+    " '--output': 'ttt.pdf'     '--drifting': None\n" + \
+    " '--version': False        '--rr': False}\n"
+    """
+
+    col1 = [' '] * rows
+    col2 = [' '] * rows
+    col3 = [' '] * rows
+    for i in range(0, rows):
+        if length > i:
+            col1[i] += insert_content(dic_list, i, rows, 0, dictionary_total)
+        if length > i + rows:
+            col2[i] += insert_content(dic_list, i, rows, 1, dictionary_total)
+        if length > i + (2 * rows):
+            col3[i] += insert_content(dic_list, i, rows, 2, dictionary_total)
+
+    return print_output_from_rows(col1, col2, col3, rows)
+
+
+# Helper function for inserting the key value pairs into output dictionary
+def insert_content(dic_list, idx, rows, col_idx, dictionary_total):
+    """
+    Args:
+        dic_list:  a dictionary the built from user argument but reform to a list.
+        idx: the current row index.
+        rows: count of the rows.
+        col_idx: index of the col,
+        dictionary_total: the dictionary that includes both keywords for output patterns
+                          and options.
+    Returns:
+        returns the key value pair in a outputting form according to the type of values.
+
+    >>> dic = {'--helping': True, '--sorted': None, '--output': 'ttt.pdf',
+    >>>        '--speed': 10, '--aaa': 20.9}
+    >>> d_list = list(dic)
+
+    >>> insert_content(dic_list=dic_list, idx=0, rows=0, col_idx=0, dictionary_total=dic)
+    '--helping: True'
+    >>> insert_content(dic_list=dic_list, idx=1, rows=0, col_idx=0, dictionary_total=dic)
+    '--sorted: None'
+    >>> insert_content(dic_list=dic_list, idx=2, rows=0, col_idx=0, dictionary_total=dic)
+    '--output: ttt.pdf'
+    >>> insert_content(dic_list=dic_list, idx=3, rows=0, col_idx=0, dictionary_total=dic)
+    '--speed: 10'
+    >>> insert_content(dic_list=dic_list, idx=4, rows=0, col_idx=0, dictionary_total=dic)
+    '--aaa: 20.9'
+    """
+
+    if check_value_type(dictionary_total[dic_list[idx + (col_idx * rows)]]):
+        return '\'{}\': {}'.format(dic_list[idx + (col_idx * rows)],
+                                   dictionary_total[dic_list[idx + (col_idx * rows)]])
+
+    return '\'{}\': \'{}\''.format(dic_list[idx + (col_idx * rows)],
+                                   dictionary_total[dic_list[idx + (col_idx * rows)]])
+
+
+# Helper method for defining whether the value is a string or a primitive type
+def check_value_type(value):
+    """
+    Args:
+        value: the value for current key in the dictionary.
+    Returns:
+        returns a boolean value whether the value passed in is primitive.
+
+     >>> check_value_type('Perfect')
+    False
+    >>> check_value_type(10)
+    True
+    >>> check_value_type(3.1415)
+    True
+    >>> check_value_type(True)
+    True
+    >>> check_value_type(None)
+    True
+    """
+
+    return isinstance(value, (int, float, bool)) or value is None
+
+
+# Helper method for printing out dictionary as a json string to user
+def print_output_from_rows(col1, col2, col3, num_rows):
+    """
+    Args:
+        col1: holds the values for output column one.
+        col2: holds the values for output column two.
+        col3: holds the values for output column three.
+        num_rows: the number of rows
+    Returns:
+        final_output: returns output string
+
+    >>> first_row = [' 11', ' 2', ' 3', ' 4', ' 5']
+    >>> second_row = [' 1', ' 222', ' 3', ' 4', ' ']
+    >>> third_row = [' 1', ' 2', ' 3333', ' ', ' ']
+    >>> print_output_from_rows(col1=col1, col2=col2, col3=col3, num_rows=5)
+    "{11     1       1\n" + \
+    " 2      222     2\n" + \
+    " 3      3       3333\n" + \
+    " 4      4\n" + \
+    " 5}\n"
+    """
+
+    spaces1 = len(max(col1, key=len))
+    spaces2 = len(max(col2, key=len))
+    final_output = ""
+    for k in range(num_rows):
+        if k == 0:
+            out = '{' + col1[k].strip().ljust(spaces1) + ' ' * 4 \
+                  + col2[k].strip().ljust(spaces2) + ' ' * 4 \
+                  + col3[k].strip().ljust(spaces2)
+        else:
+            out = col1[k].ljust(spaces1) + ' ' * 4 \
+                  + col2[k].ljust(spaces2) + ' ' * 4 \
+                  + col3[k].ljust(spaces2)
+        if k == num_rows - 1:
+            final_output += (out.rstrip() + '}\n')
+        else:
+            final_output += (out.rstrip() + '\n')
+    return final_output
--- examples/new_usage.py
+++ examples/new_usage.py
@@ -24,17 +24,19 @@ Options:
 
 """
 
+
 class Token:
-    def __init__(self, prev=None, next=None):
+    def __init__(self, prev=None, next=None, children=[]):
         self.prev = prev
         self.next = next
+        self.children = children
 
 
 class Leaf(Token):
-    def __init__(self, text, value=None, prev=None, next=None):
+    def __init__(self, text, value=None, prev=None, next=None, children=[]):
         self.text = text
         self.value = value
-        super(Leaf, self).__init__(prev, next)
+        super(Leaf, self).__init__(prev, next, children)
 
     def __repr__(self):
         return '%s(%r, %r)' % (self.__class__.__name__, self.text, self.value)
@@ -48,73 +50,136 @@ class Leaf(Token):
 
 class Argument(Leaf):
     """ Placeholder """
-    def __init__(self, text, value=None, prev=None, next=None):
-        self.value = None if len(text.strip("<>")) > 1 else 0
-        super(Argument, self).__init__(text, self.value, prev, next)
 
+    def __init__(self, text, value=None, prev=None, next=None, children=[]):
+        self.value = None if len(text.strip("<>")) > 1 else 0
+        super(Argument, self).__init__(text, self.value, prev, next, children)
+
+    def match(self, args, index):
+        if index < len(args):
+            if self.value == 0 and not is_num(args[index]):
+                print(f"Argument {self.text} not match 1")
+                return False, index+1
+            self.value = args[index]
+            return True, index+1
+        print(f"Argument {self.text} not match 2")
+        return False, index+1
 
 
 class Option(Leaf):
     """ Placeholder """
-    def __init__(self, text, value=False, short=False, long=False, prev=None, next=None):
+
+    def __init__(self, text, value=False, short=False, long=False, prev=None, next=None, children=[]):
         self.short = short
         self.long = long
         if '=' in text:
             arg = re.search('<\S+>', text).group()
-            self.value = None if len(arg.strip("<>"))>1 else 0
+            text = re.search('\S+=', text).group().strip("=")
+            self.value = None if len(arg.strip("<>")) > 1 else 0
         else:
             self.value = None if '=' in text else False
-        super(Option, self).__init__(text, self.value, prev, next)
+        super(Option, self).__init__(text, self.value, prev, next, children)
+
+    def match(self, args, index):
+        if index < len(args):
+            if self.text == args[index]:
+                self.value = True
+                return True, index+1
+        print(f"Option {self.text} not match")
+        return False, index+1
 
 
 class Command(Leaf):
     """ Placeholder """
-    def __init__(self, text, value=None, prev=None, next=None):
+
+    def __init__(self, text, value=None, prev=None, next=None, children=[]):
         self.value = False
-        super(Command, self).__init__(text, self.value, prev, next)
+        super(Command, self).__init__(text, self.value, prev, next, children)
+
+    def match(self, args, index):
+        if index < len(args):
+            if self.text == args[index]:
+                self.value = True
+                return True, index+1
+        print(f"Command {self.text} not match")
+        return False, index+1
 
 # Used for grouping Tokens by optional, required, mutex, or repeating
 
 
 class Branch(Token):
-    def __init__(self, children, prev=None, next=None):
-        self.children = children
-        super(Branch, self).__init__(prev, next)
+    def __init__(self, tokens=[], prev=None, next=None, children=[]):
+        self.tokens = tokens
+        super(Branch, self).__init__(prev, next, children)
 
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__,
-                           ', '.join(repr(a) for a in self.children))
+                           ', '.join(repr(a) for a in self.tokens))
 
     def flat(self, *types):
         if type(self) in types:
             return self
-        return [child.flat(*types) for child in self.children]
+        return [child.flat(*types) for child in self.tokens]
+
 
 class Optional(Branch):
     """ Placeholder """
 
-    def match(self, left):
-        for child in self.children:
-            match, left = child.match(left)
-        return True, left
+    # Currently only implements all-or-nothing
+    def match(self, args, index):
+        child_index = index
+        for child in self.tokens:
+            is_match, child_index = child.match(args, child_index)
+            if not is_match:
+                child_index = child_index - 1
+        return True, child_index
 
 
 class Required(Branch):
     """ Placeholder """
 
+    def match(self, args, index):
+        child_index = index
+        for child in self.tokens:
+            is_match, child_index = child.match(args, child_index)
+            if not is_match:
+                return False, index
+        return True, child_index
 
 class Mutex(Branch):
     """ Placeholder """
+
+    def match(self, args, index):
+        res, new_index = False, index
+        for child in self.tokens:
+            res, new_index = child.match(args, index)
+            if res:
+                return True, new_index
+        return False, new_index
 
 
 class Repeating(Branch):
     """ Placeholder """
 
+    # BUG: index 1 less than it should be when repeating pattern incomplete
+    def match(self, args, index):
+        is_match, new_index = self.tokens[0].match(args, index)
+        #old_index = new_index
+        if not is_match:
+            return False, new_index
+        while new_index < len(args):
+            print("Loop")
+            is_match, new_index = self.tokens[0].match(args, new_index)
+            if not is_match:
+                new_index = new_index - 1
+                break       
+        return True, new_index
+
 
 # Used for identifying branch tokens (Optional, Required, Mutex, Repeating)
 class SpecialToken(Token):
-    def __init__(self, prev=None, next=None):
-        super(SpecialToken, self).__init__(prev, next)
+    def __init__(self, prev=None, next=None, children=[]):
+        super(SpecialToken, self).__init__(prev, next, children)
 
     def __repr__(self):
         return self.__class__.__name__
@@ -153,15 +218,44 @@ class Ellipsis(SpecialToken):
 
 
 def main_function():
-    usages, options_array = docopt_util.processing_string(doc2, False, "testing new_usage")
+    usages, options_array = docopt_util.processing_string(
+        doc2, False, "testing new_usage")
     usages.pop(0)
-    usages, usage_dic = get_patterns_and_dict(usages)
+    usages, usage_dic, token_set, tree_heads = get_patterns_and_dict(usages)
+    '''print(usages)
+    print(usage_dic)
+    print(token_set)
+    print(tree_heads)'''
     args = sys.argv[1:]
+    test_arg = usages[0][0]
+    found_patt_match = False
+    patt_match = None
+    for patt_ind, pattern in enumerate(usages):
+        is_match, index = False, 0
+        found_patt_match = True
+        for token in pattern:
+            is_match, index = token.match(args, index)
+            if not is_match:
+                #print(f"NOT A MATCH\n{usages[0]}\n{args}")
+                found_patt_match = False
+                break
+        if found_patt_match:
+            #print("match")
+            patt_match = patt_ind
+            break
+    if patt_match is not None:
+        print(f"FOUND A MATCH: {patt_match}")
+        for token in usages[patt_match]:
+            print(token)
+    else:
+        print("NO MATCHES FOUND")
 
 
 def get_patterns_and_dict(usages):
     new_usages = []
     usage_dic = {}
+    token_set = []
+    tree_heads = []
     for pattern in usages:
         pattern = re.sub(r'([\[\]\(\)\|]|\.\.\.)', r' \1 ', pattern).split()
         pattern.pop(0)
@@ -169,18 +263,78 @@ def get_patterns_and_dict(usages):
         create_opt_and_req(pattern)
         create_mutex(pattern)
         create_repeating(pattern)
+        for index, token in enumerate(pattern):
+            if isinstance(token.next, SpecialToken):
+                token.next = pattern[index+1]
         new_usages.append(pattern)
         usage_dic.update(dict_populate_loop(pattern))
-    return new_usages, usage_dic
-        
+        #print(pattern[0])
+        token_set = build_token_set(pattern, token_set)
+        tree_heads = build_tree_heads(token_set, pattern[0], tree_heads)
+
+    return new_usages, usage_dic, token_set, tree_heads
+
+def is_num(arg):
+    try:
+        float(arg)
+        return True
+    except ValueError:
+        return False
+
+
+def build_tree_heads(token_set, first_token, tree_heads):
+    for token in token_set:
+        #print(token, first_token)
+        if not isinstance(first_token, Branch):
+            if token == first_token:
+                tree_heads.append(token)
+                return tree_heads
+        elif isinstance(first_token, Mutex):
+            for child in first_token.tokens:
+                #print(child)
+                if token.text == child.text:
+                    tree_heads.append(token)
+                    break
+        else:
+            old_len = len(tree_heads)
+            tree_heads = build_tree_heads(token_set, first_token.tokens[0], tree_heads)
+            if len(tree_heads) > old_len:
+                return tree_heads
+    return tree_heads
+
+def build_token_set(pattern, token_set):
+    for token in pattern:
+        tree_child = token.next if token.next else None
+        if isinstance(token, Branch):
+            token_set = build_token_set(token.tokens, token_set)
+        else:
+            in_set = False
+            for t in token_set:
+                if token.text == t.text:
+                    token = t
+                    in_set = True
+                    break
+            if not in_set:
+                token_set.append(token)
+            if tree_child:
+                #print(f"Child added to {token}: {tree_child}\t{hex(id(token))}")
+                token.children.append(tree_child)
+
+            #if token.text == pattern[0].text:
+             #   tree_heads.append(token)
+
+    return token_set
+
+
 def dict_populate_loop(pattern):
     updated_dic = {}
     for token in pattern:
         if isinstance(token, Branch):
-            updated_dic.update(dict_populate_loop(token.children))
+            updated_dic.update(dict_populate_loop(token.tokens))
         else:
             updated_dic[token.text] = token.value
     return updated_dic
+
 
 def identify_tokens(pattern):
     new_pat = []
@@ -216,6 +370,7 @@ def identify_tokens(pattern):
             token.next = new_pat[index+1]
     return new_pat
 
+
 def create_opt_and_req(pattern):
     length = len(pattern)-1
     for index, token in enumerate(pattern[::-1]):
@@ -235,18 +390,24 @@ def create_opt_and_req(pattern):
                     break
                 collected.append(x)
                 del pattern[index]
-            res = Required(collected, prev, next) if isinstance(token, Required_Open) else Optional(collected, prev, next)
+            collected[0].prev = prev
+            collected[-1].next = next
+            res = Required(collected, prev, next) if isinstance(
+                token, Required_Open) else Optional(collected, prev, next)
             pattern.insert(index, res)
 
 
 def create_mutex(pattern):
     for index, token in enumerate(pattern):
         if isinstance(token, Optional) or isinstance(token, Required):
-            create_mutex(token.children)
+            create_mutex(token.tokens)
         elif isinstance(token, Pipe):
             prev = token.prev.prev if token.prev else None
             next = token.next.next if token.next else None
             collected = [token.prev, token.next]
+            for tok in collected:
+                tok.prev = prev
+                tok.next = next
             for i in range(index-1, index+2):
                 del pattern[index-1]
             res = Mutex(collected, prev, next)
@@ -258,8 +419,9 @@ def create_repeating(pattern):
         prev = token.prev if token.prev else None
         next = token.next if token.next else None
         if isinstance(token, Optional) or isinstance(token, Required) or isinstance(token, Mutex):
-            create_repeating(token.children)
+            create_repeating(token.tokens)
         elif isinstance(token, Ellipsis):
+            token.prev.next = next
             collected = [token.prev]
             res = Repeating(collected, prev, next)
             for i in range(index-1, index+1):
@@ -269,4 +431,3 @@ def create_repeating(pattern):
 
 if __name__ == "__main__":
     main_function()
-    
