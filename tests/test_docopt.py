dc88c60a3634fd99c0dcc22e24b34d8279140a04
Chengen Li <aojie@vt.edu>
2021-04-13 16:50:43 -0400

    reorganize the files and functions

--- tests/test_docopt.py
+++ tests/test_docopt.py
@@ -21,7 +21,7 @@ Options:
 """
 
 options = """Options:
-  -h --help --helping  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -43,7 +43,7 @@ Usage:
   naval_fate.py --version
 
 Options:
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -65,7 +65,7 @@ doc2 = """Usage:
   naval_fate.py --version
 
 Options:
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -98,7 +98,7 @@ Usage:
   naval_fate.py (-h | --help)
   naval_fate.py --version
 
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -121,7 +121,7 @@ Usage:
   naval_fate.py (-h | --help)
   naval_fate.py --version
 
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -146,7 +146,7 @@ usage = """Usage:
   naval_fate.py (-h | --help)
   naval_fate.py --version"""
 
-options_2 = """  -h --help --helping --haha -hhh --ooooo  Show this screen.
+options_2 = """  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -179,7 +179,7 @@ Usage:
   naval_fate.py --version
 
 Options:
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -208,7 +208,7 @@ Usage:
   naval_fate.py --version
 
 Options:
-  -h --help --helping --haha -hhh --ooooo  Show this screen.
+  -h --help Show this screen.
   --sorted  Show sorted.
   -o FILE --output=<value>  Speed in knots [default: ./test.txt].
   --version     Show version.
@@ -270,8 +270,6 @@ def test_processing_string(capsys):
         doc=doc1, help_message=False, version="test 2.0")
     assert usage.split('\n') == usage_array
     assert options.split('\n') == options_array
-    print(usage_array)
-    print(options_array)
 
     res = docopt.processing_string(doc=None, help_message=False, version="test 2.0")
     assert res is None
@@ -291,7 +289,6 @@ def test_get_usage_and_options():
     assert tmp_name == name
     assert tmp_usage == usage
     assert tmp_options == options
-    print(display)
 
     tmp_name, tmp_usage, tmp_options, display = \
         docopt.get_usage_and_options(doc=doc2, version=version)
@@ -309,8 +306,6 @@ def test_get_usage_and_options():
         docopt.get_usage_and_options(doc=doc4, version=version)
     assert tmp_name == ""
     assert tmp_usage == usage
-    print(tmp_options)
-    print(options_2)
     assert tmp_options == options_2
 
     tmp_name, tmp_usage, tmp_options, display = \
@@ -649,21 +644,16 @@ def find_default_value():
 ##############################################################################
 # Test print function
 def test_print_output_dictionary():
-    input2 = {'1': True, '2': 'haha', '3': False, '4': True, '5': 'haha', '6': False,
-              '7': True, '8': 'haha', '9': False, '10': True, '11': 'haha', '12': False}
-    input3 = {'1': True, '2': 'haha', '3': False, '4': True, '5': 'haha', '6': False,
+    input1 = {'1': True, '2': 'haha', '3': False, '4': True, '5': 'haha', '6': False,
               '7': True, '8': 'haha', '9': False, '10': True, '11': 'haha', '12': False,
               '13': True, '14': 'haha', '15': False, '16': True, '17': 'haha', '18': False,
               '19': True, '20': 'haha', '21': False, '22': True, '23': 'haha', '24': False,
               '25': True, '26': 'haha', '27': False, '28': True, '29': 'haha', '30': False}
     usage_dic = {'usage1': 'x', 'usage2': 'y'}
-    options_dic = {'options1': 'x', 'options2': 'y'}
     dic_total, res = docopt.print_output_dictionary(usage_dic=usage_dic)
     assert dic_total == {**usage_dic, **dic_total}
-    dic_total, res = docopt.print_output_dictionary(usage_dic={})
-    assert dic_total == input2
-    dic_total, res = docopt.print_output_dictionary(usage_dic=input3)
-    assert dic_total == {**input3, **options_dic}
+    dic_total, res = docopt.print_output_dictionary(usage_dic=input1)
+    assert dic_total == input1
 
 
 # Test function for testing the function for outputting a correct format of dictionary according
