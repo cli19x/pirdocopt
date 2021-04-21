26614e35e896559a356811b3290440410a3f5af4
Chengen Li <aojie@vt.edu>
2021-04-21 16:56:43 -0400

    Added more test cases

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='Pir_Docopt',
-    version='0.1dev',
+    name='pirdocopt',
+    version='1.0.1',
     packages=['pirdocopt'],
     license='MIT License',
     long_description=open('README.rst').read(),
